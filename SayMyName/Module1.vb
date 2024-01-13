40da2e47f8640f716141454797d4b0dfec5fdaea
AlexWheelock <130195109+AlexWheelock@users.noreply.github.com>
2024-01-12 18:02:16 -0700

    Got working

--- SayMyName/Module1.vb
+++ SayMyName/Module1.vb
@@ -1,6 +1,22 @@
-﻿Module Module1
+﻿'Alex Wheelock
+'RCET 0265
+'Spring 2024
+'Say My Name
+'https://github.com/AlexWheelock/SayMyName.git
+
+Module Module1
 
     Sub Main()
+
+        Dim userInput As String
+
+        Console.WriteLine("What is your name?") 'asks the user for their name
+        Console.WriteLine("Please enter your name")
+
+        userInput = Console.ReadLine
+        Console.WriteLine("Hello," & " " & userInput & "!")
+
+        Console.Read()
 
     End Sub
 
