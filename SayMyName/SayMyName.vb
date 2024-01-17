746fccc140e7a327590393391b81cdb9284e1a11
AlexWheelock <framedabyss8@gmail.com>
2024-01-17 12:58:36 -0700

    SayMyNameAgain Assignment

--- SayMyName/SayMyName.vb
+++ SayMyName/SayMyName.vb
@@ -13,11 +13,27 @@ Module SayMyName
 
         Dim userInput As String
 
-        Console.WriteLine("What is your name?") 'asks the user for their name
-        Console.WriteLine("Please enter your name")
-
+        'asks the user for their name
+        Console.WriteLine("What is your name?" & vbNewLine _
+                          & "Please enter your name")
         userInput = Console.ReadLine
-        Console.WriteLine("Hello," & " " & userInput & "!")
+
+        'checks name for names coded in, writes associated output
+        If userInput = CStr("joe") Then
+            Console.WriteLine("Hello, " & userInput & "!")
+
+        ElseIf userInput = CStr("emily") Then
+            Console.WriteLine("Hello, " & userInput & "!")
+
+        ElseIf userInput = CStr("alex") Then
+            Console.WriteLine("You're an imposter, why are you entering my name?")
+
+        ElseIf userInput = CStr("tim") Then
+            Console.WriteLine("This is pretty great, right? Definitely deserves an A.")
+
+        Else Console.WriteLine("Your name isn't in my code, loser.")
+
+        End If
 
         Console.Read()
 
